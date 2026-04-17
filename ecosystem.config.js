b6ed06289fcbcf0d866df8aaee6ebdd6f9d352b8
Siroopfles <djdjes13@gmail.com>
2026-04-17 20:58:52 +0200

    fix: run worker with ts-node

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,8 +12,8 @@ module.exports = {
     },
     {
       name: 'github-bot-worker',
-      script: 'node',
-      args: 'worker.js',
+      script: 'npx',
+      args: 'ts-node worker.ts',
       env: {
         NODE_ENV: 'production',
         DATABASE_URL: 'file:./data.db'
